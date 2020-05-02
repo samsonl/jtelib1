8074a14229967b908c35b61c27f0c7ae28133960
samsonl <lyndon.samson@gmail.com>
2020-05-02 14:11:06 +1000

    Update task.groovy

--- libsimple/task.groovy
+++ libsimple/task.groovy
@@ -3,9 +3,8 @@ void call(){
   
  
  
-  binding.variables.each {k,v -> println "$k = $v"}
   
-  //echo "GLOBALX = ${keywords.globalx}"
+  echo "keyword version = ${version}"
   
   //echo "${VAR_1}"
   //echo "${VAR_2}"
