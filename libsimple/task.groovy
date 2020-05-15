0f07f71a781474774d07e02c0d90548dd71877d4
samsonl <lyndon.samson@gmail.com>
2020-05-15 19:31:10 +1000

    Update task.groovy

--- libsimple/task.groovy
+++ libsimple/task.groovy
@@ -4,7 +4,7 @@ void call(){
   echo "global.A = ${global.A}"
 
   printf ">>>${global.class}"
-  printf ">>>${global.@config}"
+  printf ">>>${global.@config.class}"
   
   def field = global.class.getDeclaredField( "config" );
   field.setAccessible( true );
