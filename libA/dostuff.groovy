47cd7a5a9341742fd393e511e1ffcbb45e9e4eea
samsonl <lyndon.samson@gmail.com>
2020-05-19 19:06:21 +1000

    Update dostuff.groovy

--- libA/dostuff.groovy
+++ libA/dostuff.groovy
@@ -10,4 +10,9 @@ def call() {
   echo "libA dostuff"
   echo "Task.localC = ${task.getLocalC()}"
   
+  def hel = new util.Helper()
+  hel.helpme();
+  println ">> CLASS:${hel.getClass()}"
+  
+  
 }
