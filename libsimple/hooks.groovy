0828579fe2a31facbf8b8d3b93a87452bd2b28a4
samsonl <lyndon.samson@gmail.com>
2020-07-03 22:20:43 +1000

    Update hooks.groovy

--- libsimple/hooks.groovy
+++ libsimple/hooks.groovy
@@ -1,1 +1,27 @@
-scdf
+
+@Init
+def init(context) {
+  echo "Init"
+}
+
+@BeforeStep
+def before(context) {
+  echo "Before"
+}
+
+@AfterStep
+def after(context) {
+  echo "After"
+}
+
+@CleanUp
+def clean(context) {
+  echo "Clean"
+}
+
+@Notify
+def notify(context) {
+  echo "Notify"
+}
+
+
