4af469623de05370039a1a5d917634260892967e
samsonl <lyndon.samson@gmail.com>
2020-08-14 22:51:57 +1000

    Update lifecycle.groovy

--- testmanager/lifecycle.groovy
+++ testmanager/lifecycle.groovy
@@ -1,18 +1,18 @@
 @Init
 void call1(context){
-  echo "lc:Init ${context}"
+  //echo "lc:Init ${context}"
 }
 @BeforeStep
 void call2(context){
-  echo "lc:BeforeStep ${context}"
+  //echo "lc:BeforeStep ${context}"
 }
 @AfterStep
 void call3(context){
-  echo "lc:AfterStep ${context}"
+  //echo "lc:AfterStep ${context}"
 }
 @CleanUp
 void call4(context){
-  echo "lc:CleanUp ${context}"
+  //echo "lc:CleanUp ${context}"
 }
 @Notify
 void call5(context){
